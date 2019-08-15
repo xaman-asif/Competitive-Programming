73c1860ceace4741f85cb2d76436987272dad360
Asifuzzaman Opi <51073544+0ne1ronaut@users.noreply.github.com>
2019-08-15 14:30:45 +0600

    Create 451A - Game With Sticks.cpp

---
+++ Codeforces/Div2-A/451A - Game With Sticks.cpp
@@ -0,0 +1,23 @@
+/*
+ * Author:      Asifuzzaman Opi
+ * Handle:      0ne1ronaut
+ * University:  United International University
+ * Email:       asifuzzaman.opi@gmail.com
+ */
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(0); cout.tie(0);
+  int n, m, win = 0;
+  cin >> n >> m;
+  while(n && m)
+  {
+    n--;
+    m--;
+    win++;
+  }
+  (win%2==0)?cout<<"Malvika\n":cout<<"Akshat\n";
+  return 0;
+}
