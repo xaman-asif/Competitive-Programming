06b42c777cf84ea03def22475d4f0a2090bf662c
Asifujjaman Opi <asifujjaman.opi@bracits.com>
2024-10-14 15:03:22 +0600

    Leetcode (10/150)

---
+++ Leetcode/src/main/java/com/problemsolving/ValidPalindrome.java
@@ -0,0 +1,15 @@
+package com.problemsolving;
+
+public class ValidPalindrome {
+  public boolean isPalindrome(String s) {
+    s = s.toLowerCase().replaceAll("[^a-z0-9]", "");
+
+    for (int i = 0; i < s.length() / 2; i++) {
+      if (s.charAt(i) != s.charAt(s.length() - 1 - i)) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+}
