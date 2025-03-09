ee77eec69ffc1cf880924bdc6d29aa9182fba3b8
Asifujjaman Opi <asifujjaman.opi@bracits.com>
2025-03-09 17:19:47 +0600

    add solutions for Pascal's Triangle and Majority Element II in Java

---
+++ Leetcode/18. 4Sum.java
@@ -0,0 +1,137 @@
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Set;
+
+public class Main {
+  private static final class tokens extends LinkedList<String> {
+    tokens(String line) {
+      super(new LinkedList<String>());
+      for (var token : line.split(" ")) {
+        add(token);
+      }
+    }
+
+    String nextStr() {
+      return remove();
+    }
+
+    int nextInt() {
+      return Integer.parseInt(remove());
+    }
+
+    long nextLong() {
+      return Long.parseLong(remove());
+    }
+
+    double nextDbl() {
+      return Double.parseDouble(remove());
+    }
+
+    BigInteger nextBigInt() {
+      return new BigInteger(remove());
+    }
+  }
+
+  private static tokens Scanner(BufferedReader cin) {
+    String line = null;
+    try {
+      line = cin.readLine();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    return new tokens(line);
+  }
+
+  private static void print(String s) {
+    try {
+      SystemOut.write(s.getBytes());
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  private static void print(Integer i) {
+    print(i.toString());
+  }
+
+  private static void print(Long l) {
+    print(l.toString());
+  }
+
+  private static void print(String f, Object... args) {
+    print(String.format(f, args));
+  }
+
+  private static void print(BigInteger ii) {
+    print(ii.toString());
+  }
+
+  private static void fileIO() throws FileNotFoundException {
+    SystemOut = new BufferedOutputStream(new FileOutputStream("CP/outputf.in"));
+    SystemIn = new BufferedReader(new InputStreamReader(new FileInputStream("CP/inputf.in")));
+  }
+
+  private static BufferedReader SystemIn = new BufferedReader(new InputStreamReader(System.in));
+  private static BufferedOutputStream SystemOut = new BufferedOutputStream(System.out);
+  private final static String space = " ";
+  private final static String endl = "\n";
+
+  public static void main(String[] args) throws IOException {
+    // fileIO();
+    // var sc = Scanner(SystemIn);
+    // int n = sc.nextInt();
+    Main main = new Main();
+
+    main.fourSum(new int[] {1000000000,1000000000,1000000000,1000000000}, -294967296);
+
+    SystemOut.flush();
+  }
+
+  public List<List<Integer>> fourSum(int[] nums, int target) {
+    Arrays.sort(nums);
+
+    Set<List<Integer>> result = new HashSet<>();
+
+    for (int i = 0; i < nums.length; i++) {
+      for (int j = i + 1; j < nums.length; j++) {
+
+        int left = j + 1;
+        int right = nums.length - 1;
+
+        while (left < right) {
+          long sum = nums[i];
+          sum += nums[j];
+          sum += nums[left];
+          sum += nums[right];
+
+          if (sum == target) {
+            result.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
+            while (left < right && nums[left] == nums[left + 1]) left++;
+            while (left < right && nums[right] == nums[right - 1]) right--;
+            left++;
+            right--;
+          } else if (target - sum > 0) {
+            left++;
+          } else {
+            right--;
+          }
+        }
+      }
+    }
+
+    return result.stream().toList();
+  }
+}
